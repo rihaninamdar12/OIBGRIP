a27876bc221f4444d2ce09103761bfce14c401ed
Rihan <rihaninamdar12@gmail.com>
2022-09-23 18:28:12 +0530

    Initial commit

--- Task 3/JS/script.js
+++ Task 3/JS/script.js
@@ -1,60 +1,65 @@
-function convertion()
-{
-    let Value=document.getElementById("degree").value;
-    let display_result=0;
-    let selected_type=document.getElementById("type").value;
-    let result=0;
-    if(isNaN(Value))
-    {
+function convertion() {
+    // Accessing User input
+    let Value = document.getElementById("degree").value;
+
+    // Declaring variable for o/p
+    let display_result = 0;
+
+    // Accessing Temperature type
+    let selected_type = document.getElementById("type").value;
+
+    let result = 0;
+
+    // Validating user input 
+    if (isNaN(Value)) {
         alert("Enter a Valid Number");
     }
-    else
-    {
-        Value=Number.parseInt(Value);
+    else {
+        Value = Number.parseInt(Value);
     }
 
-    if(selected_type=="C-F" && !isNaN(Value))
-    {
-        result=((Value*9/5)+32).toFixed(4);
-        result=result+" ℉";
-        display_result=document.getElementById("output").value=result;
+    // Conversion of Celcius to Fahrenheit
+    if (selected_type == "C-F" && !isNaN(Value)) {
+        result = ((Value * 9 / 5) + 32).toFixed(4);
+        result = result + " ℉";
+        display_result = document.getElementById("output").value = result;
     }
 
-    else if(selected_type=="C-K" && !isNaN(Value))
-    {
-        result=(Value+273.15).toFixed(4);
-        result=result+" K";
-        display_result=document.getElementById("output").value=result;
+    // Conversion of Celcius to Kelvin
+    else if (selected_type == "C-K" && !isNaN(Value)) {
+        result = (Value + 273.15).toFixed(4);
+        result = result + " K";
+        display_result = document.getElementById("output").value = result;
     }
 
-    else if(selected_type=="F-C" && !isNaN(Value))
-    {
-        result=((Value-32)*5/9).toFixed(4);
-        result=result+" ℃";
-        display_result=document.getElementById("output").value=result;
-    }
-    
-    else if(selected_type=="F-K" && !isNaN(Value))
-    {
-        result=(((Value-32)*5/9)+273.15).toFixed(4);
-        result=result+" K";
-        display_result=document.getElementById("output").value=result;
+    // Conversion of Fahrenheit to Celcius
+    else if (selected_type == "F-C" && !isNaN(Value)) {
+        result = ((Value - 32) * 5 / 9).toFixed(4);
+        result = result + " ℃";
+        display_result = document.getElementById("output").value = result;
     }
 
-    else if(selected_type=="K-C" && !isNaN(Value))
-    {
-        result=(Value-273.15).toFixed(4);
-        result=result+" ℃";
-        display_result=document.getElementById("output").value=result;
+    // Conversion of Fahrenheit to Kelvin
+    else if (selected_type == "F-K" && !isNaN(Value)) {
+        result = (((Value - 32) * 5 / 9) + 273.15).toFixed(4);
+        result = result + " K";
+        display_result = document.getElementById("output").value = result;
     }
 
-    else if(selected_type=="K-F" && !isNaN(Value))
-    {
-        result=(((Value-273.15)*9/5)+32).toFixed(4);
-        result=result+" ℉";
-        display_result=document.getElementById("output").value=result;
+    // Conversion of Kelvin to Celcius
+    else if (selected_type == "K-C" && !isNaN(Value)) {
+        result = (Value - 273.15).toFixed(4);
+        result = result + " ℃";
+        display_result = document.getElementById("output").value = result;
     }
-    
 
-    
+    // Conversion of Kelvin to Fahrenheit
+    else if (selected_type == "K-F" && !isNaN(Value)) {
+        result = (((Value - 273.15) * 9 / 5) + 32).toFixed(4);
+        result = result + " ℉";
+        display_result = document.getElementById("output").value = result;
+    }
+
+
+
 }
